3e88edb7cfedeb83b139d41e78136fe2195571b6
maxsk8er <sk8er999@gmail.com>
2021-02-24 22:04:38 +0000

    reviews

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+	return (
+		<>
+			{people.map((person) => {
+				const { id, name, age, image } = person
+				return <article key={id} className='person'>
+					<img src={image} alt={name} />
+					<div>
+						<h4>{name}</h4>
+						<p>{age} years</p>
+					</div>
+				</article>
+			})}
+		</>
+	);
 };
 
 export default List;
--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+
+	return <section>
+		<div className='title'>
+			<h2>our tours</h2>
+			<div className='underline'></div>
+			<div>
+				{tours.map((tour) => {
+					return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+				})}
+			</div>
+		</div>
+	</section>;
 };
 
 export default Tours;
